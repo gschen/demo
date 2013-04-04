6259d9e42fa778ef648c515f804e80f2f7bc1fbb
gschen <gschen.cn@gmail.com>
2013-04-04 11:58:41 +0800

    first complete project organization.

---
+++ demo/src/main/java/edu/fudan/demo/controller/UserController.java
@@ -0,0 +1,26 @@
+package edu.fudan.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.fudan.demo.pojo.User;
+
+@Controller
+public class UserController {
+
+	@RequestMapping(value="userlist")
+	public String test(Model model){
+		
+		List<User> userlist = new ArrayList<User>();
+		userlist.add(new User("aaaa", "222"));
+		
+		userlist.add(new User("bbbb", "33333"));
+		
+		model.addAttribute("users", userlist);
+		return "userlist";
+	}
+}
---
+++ demo/src/main/java/edu/fudan/demo/pojo/User.java
@@ -0,0 +1,24 @@
+package edu.fudan.demo.pojo;
+
+public class User {
+
+	private String username;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String password;
+	
+	public User(String u, String p){
+		this.username = u;
+		this.password = p;
+	}
+}
